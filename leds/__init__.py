5ceb187d99565fee4e0258fbb56c9497463ca7cb
Peter Züger <zueger.peter@icloud.com>
2025-09-02 16:40:24 +0200

    replaced NoEffect with None
    no need to allocate an empty asyncio Task

--- leds/__init__.py
+++ leds/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env micropython
 import asyncio
 
-from .effects import NoEffect
 from .color import RGB
 
 
@@ -29,27 +28,24 @@ class _LedBase:
     def brightness(self, _):
         raise NotImplementedError
 
-    def effect(self, effect=NoEffect()):
+    def effect(self, effect=None):
         self.cancel()
-        if isinstance(effect, tuple):
-            self._effect = asyncio.create_task(
-                asyncio.gather(*[e.run(self) for e in effect])
-            )
-        else:
+        if effect is not None:
             self._effect = asyncio.create_task(effect.run(self))
 
     def cancel(self):
         if self._effect:
             self._effect.cancel()
+            self._effect = None
 
     def done(self):
-        return self._effect.done()
+        return (self._effect is None) or self._effect.done()
 
 
 class MultiLed:
     def __init__(self, leds):
         self.leds = leds
-        self._effect = asyncio.create_task(NoEffect().run(None))
+        self._effect = None
 
     def cancel_all(self):
         for led in self.leds:
@@ -73,17 +69,15 @@ class MultiLed:
         for led in self.leds:
             led.brightness = value
 
-    def effect(self, effect=NoEffect()):
-        self._effect.cancel()
-        if isinstance(effect, tuple):
-            self._effect = asyncio.create_task(
-                asyncio.gather(*[e.run(self) for e in effect])
-            )
-        else:
+    def effect(self, effect=None):
+        self.cancel()
+        if effect is not None:
             self._effect = asyncio.create_task(effect.run(self))
 
     def cancel(self):
-        self._effect.cancel()
+        if self._effect:
+            self._effect.cancel()
+            self._effect = None
 
     def done(self):
-        return self._effect.done()
+        return (self._effect is None) or self._effect.done()
